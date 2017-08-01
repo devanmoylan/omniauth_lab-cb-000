7528dd18ea8acf26cfceee64e5c6648b1e6fbdd5
Devan <devan.moylan@backyardlion.com>
2017-08-01 16:21:22 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    @user = User.find_or_create_by(uid: auth_hash[:uid]) do |u|
+      u.name = auth_hash[:info][:name]
+    end
+    session[:user_id] = @user.id
+    redirect_to '/'
   end
 
-  def destroy
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
